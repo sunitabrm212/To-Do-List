739f21083787a06dd4986a010c5a1028e759577e
sunita baram <sunitabaram489@gmail.com>
2025-07-12 15:48:14 +0545

    to-do-list created with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const onToDoInputChange = (event) => {
+    setTodo(event.target.value);
+  }
+
+  const onAddToDoList = () => {
+    setTodoList([...todoList, {id : uuid(), task: todo, isCompleted: false}]);
+    setTodo("");
+    console.log(todoList);
+  }
+
+  const onDeleteClick = (id) => {
+    const updatedList = todoList.filter(todo => todo.id !== id);
+    setTodoList(updatedList);
+  }
+
+  const onCheckboxChange = (id) => {
+    const updatedList = todoList.map(todo => todo.id === id ? {...todo, isCompleted : !todo.isCompleted} : todo);
+    setTodoList(updatedList);
+    console.log(updatedList);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>YOUR TO-DO-LIST</h1>
+      <div className = "form-task">
+        <input className = "task-input" value = {todo} onChange = {onToDoInputChange} placeholder = "Add your task here..."/>
+        <button className = "add-btn" onClick = {onAddToDoList}>ADD</button>
+      </div>
+      <div>
+        {
+          todoList.length > 0 && todoList.map(todo => (
+            <div key = {todo.id} className = "to-do-list">
+              <label>
+                <input className = "check-box" type = "checkbox" onChange = {() => onCheckboxChange(todo.id)} />
+                <span className = {todo.isCompleted ? 'strike' : ''}>{todo.task}</span>
+              </label>
+              <button className = "del-icon" onClick = {() => onDeleteClick(todo.id)}><FontAwesomeIcon icon = {faTrash}/></button>
+            </div>
+          ))
+        }
+      </div>
     </div>
   );
 }
